900b2bd6696620b2f5cf6c9eec54739072f64fbe
Cal <20716204+calw20@users.noreply.github.com>
2020-12-24 10:03:37 +1000

    Update Stepper Tests

--- src/system_tests/stepper_tests.cpp
+++ src/system_tests/stepper_tests.cpp
@@ -1,8 +1,6 @@
 #include "stepper_tests.h"
 
 bool stepperTestRotation(Stepper testStepper, long int steps, unsigned long timeout){
-    DBG_FPRINTLN("[Stepper Test] Full Rotation Test");
-    DBG_FPRINTLN("[WARNING] This MAY expose samples to air.");
     DBG_PRINTFN("[Stepper Test] Rotating %ld steps...", steps);
     testStepper.rotateAndSignal(steps);
     DBG_FPRINTLN("[Stepper Test] Rotated, wating for timout...");
@@ -15,16 +13,19 @@ bool stepperTestRotation(Stepper testStepper, long int steps, unsigned long time
 }
 
 bool stepperTestFullRotation(Stepper testStepper){
+    DBG_FPRINTLN("[Stepper Test] Full Rotation Test");
     DBG_FPRINTLN("[WARNING] This WILL expose ALL samples to air.");
     return stepperTestRotation(testStepper, ALT_SAMPLE_COUNT*STEPS_PER_ROTATION, TEST_STEP_TIMEOUT);
 }
 
 bool stepperTestOneRotation(Stepper testStepper){
+    DBG_FPRINTLN("[Stepper Test] One Rotation Test");
     DBG_FPRINTLN("[WARNING] This WILL expose at least ONE sample to air.");
     return stepperTestRotation(testStepper, STEPS_PER_ROTATION, TEST_STEP_TIMEOUT);
 }
 
 bool stepperTestTinyRotation(Stepper testStepper){
+    DBG_FPRINTLN("[Stepper Test] Tiny Rotation Test");
     DBG_FPRINTLN("[WARNING] This MAY expose some samples to air.");
     return stepperTestRotation(testStepper, TEST_STEPS_FOR_TINY_ROTATION, TEST_STEP_TIMEOUT);
 }
