7852776943e0ccfdade4c576901175757b421dcf
Cal <20716204+calw20@users.noreply.github.com>
2020-12-25 21:20:25 +1000

    Begin Fan Controller Tests

--- src/system_tests/fan_tests.cpp
+++ src/system_tests/fan_tests.cpp
@@ -1,5 +1,16 @@
 #include "fan_tests.h"
 
-bool fanTest(FanController testController, unsigned long timeout){
+bool fanTest(FanController testController, long fanSpeed, unsigned long timeout){
+    DBG_FPRINT_SVLN("[EDF & Controller Test] Setting a fan speed of: ", fanSpeed);
+    testController.setSpeed(fanSpeed);
+    DBG_FPRINT_SVLN("[EDF & Controller Test] Running EDF's for a time (ms) of: ", timeout);
+    unsigned long startTime = millis();
+    while (millis() - startTime < timeout){
+        testController.writeSpeed();
+    }
+    testController.setSpeed(0);
+    testController.writeSpeed();
+    DBG_FPRINTLN("[EDF & Controller Test] Test Complete. System should be reset to original setings.");
+
     return true;
 }
